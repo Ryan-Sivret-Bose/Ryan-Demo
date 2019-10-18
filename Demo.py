f2e6fe0530668a5b40dba036626b3be6b9cf039d
Ryan Sivret <Ryan_Sivret@bose.com>
2019-10-18 10:39:16 -0400

    addContact - Changed to use a string instead of list for entry into file, added try and except blocks for user error. Menu - Added try and except block to account for user error Signed-off-by: Ryan Sivret <Ryan_Sivret@bose.com>

--- Demo.py
+++ Demo.py
@@ -7,7 +7,7 @@ def readData(fname):
     for line in fh:
         print(line)
     fh.close
-
+#Print contents of the address book
 def readAddressBook(fname):
     fh = open(fname,"r")
     file_contents = fh.read()
@@ -18,6 +18,7 @@ def writeData(data):
     fh = open(fname,'w')
     f.write(str(data))
 
+#Find for search term and print the line from the file
 def search(fname):
     fh = open(fname, 'r')
     choice = input('Enter in a piece of information about this person: ')
@@ -26,77 +27,61 @@ def search(fname):
     for i, line in enumerate(searchlines):
         if choice in line:
             print(line)
-                
-    #c = list(contact)
-    #for line in range(0, len(list)):
-    #    if choice in line:
-    #        print(line)
 
-
-#def searchstreet(street):
-    #fh=open(fname)
-    #for line in fh:
-
+#Ask for contact information, final verification and entry into file.
 def addContact():
     name = input("First Name? ")
-    contact = []
-    contact.append(name)
+    contact = "placeholder"
+    contact = name + ","
     lname = input('Last Name? ')
-    contact.append(lname)
+    contact = contact + lname + ","
     street = input("What is their street address? ")
-    contact.append(street)
+    contact = contact + street + ","
     city = input("What city/town do they live in? ")
-    contact.append(city)
+    contact = contact + city + ","
     state = input("What state do they live in? ")
-    contact.append(state)
+    contact = contact + state + ","
     zip1 = input("What is zipcode of where they live? ")
-    contact.append(zip1)
-    fh = open(fname,'a')
-    fh.write(','.join(map(str, contact)))
-    fh.write('\n')
-    fh.close
-
-    #str(contact).strip('[]')
-   #','.join(map(str, contact)) 
-
+    contact = contact + zip1 
+    print(contact)
+    choice = input("Is this the correct contact information Y/N?")
+    try:
+        if choice == 'Y':
+            fh = open(fname,'a')
+            fh.write(contact + '\n')
+            fh.close
+        elif choice == 'N':
+            addContact()
+        elif choice != 'N' or 'Y':
+            quit()
+    except:   
+        print('Sorry, Did not recognize that entry, please try again' )
+        choice = input('1 - Return to the Main Menu, 2 - Return to add contact: ')
+        if choice == '1':
+            Menu()
+        elif choice == '2':
+            addContact()
 
 def Menu():
     #Choice of adress book and Selection of Task
     choice = input('Enter 1 to look at your Contacts, Enter 2 to Search for a Contact, Enter 3 to Add a Contact, Enter 4 to Delete a Contact, Enter 5 to Exit: ')
-    if choice == '1':
-        readAddressBook(fname)
+    try:
+        if choice == '1':
+            readAddressBook(fname)
+            Menu()
+        elif choice == '2':
+            search(fname)
+            Menu()
+        elif choice == '3':
+            addContact()
+            Menu()
+        #elif choice == '4':
+        elif choice == '5':
+            print("All Done!")
+        elif choice != '1' or '2' or '3' or '4':
+            quit()
+    except:
+        print("Sorry, Did not recognize that entry, please try again")
         Menu()
-    elif choice == '2':
-        search(fname)
-        Menu()
-    elif choice == '3':
-        addContact()
-        Menu()
-    #elif choice == '4':
-    elif choice == '5':
-        quit()
 
 Menu()
-
-
-
-
-
-
-
-
-#fname = input("Enter an Address Book you would like to read: ")
-#name = input("Enter a name you are lookin for: ")
-#searchname(name)
-#addContact()
-
-
-
-
-
-#f=open('data.txt','w')
-#f.write('this is some sample data...')
-#f.close()
-
-
-
